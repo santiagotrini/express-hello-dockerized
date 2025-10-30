5968146e556361a51e3adacfb2439fb4c41173cd
Santiago Trini <santiagotrini@gmail.com>
2025-10-30 12:09:01 -0300

    bad bad bad fix

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const conn = mysql.createConnection({
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
+  //flags: ['SSL','SSL_VERIFY_SERVER_CERT']
 });
 
 conn.connect(err => {
