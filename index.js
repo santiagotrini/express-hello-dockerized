5bc2fb621129818eaa2df8a7edf6346edaf8e204
Santiago Trini <santiagotrini@gmail.com>
2025-10-30 11:55:33 -0300

    agrega db

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import express from 'express';
+import mysql from 'mysql';
+import 'dotenv/config';
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+conn.connect(err => {
+  if (err) throw err;
+  console.log(`DB connected @ ${process.env.DB_HOST}`);
+});
+
 
 const app = express();
 
